25f19c37f594ae1fde52e58f02b86c0641ee1183
AaravKashyap <47911860+AaravKashyap@users.noreply.github.com>
2022-11-20 18:34:11 -0800

    good

--- olympweb/src/index.js
+++ olympweb/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -15,3 +16,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
